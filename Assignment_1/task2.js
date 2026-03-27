46bd9db1a76f7b6ca42d20e664480920a2b6c6fe
Abhinandan <abhinandanhegde1604@gmail.comgit config --global user.emailgit config --global user.name git config --global user.email abhinandanhegde1604@gmail.comgit config --global user.email abhinandanhegde1604@gmail.com>
2026-03-27 20:29:15 +0530

    Assignment 1 Task 2 - Arrow functions

---
+++ Assignment_1/task2.js
@@ -0,0 +1,10 @@
+const greet = (name) => {
+    return "Hello, " + name + "!";
+};
+
+const add = (a, b) => {
+    return a + b;
+};
+
+console.log(greet("Abhinandan"));
+console.log("Sum:", add(10, 5));
