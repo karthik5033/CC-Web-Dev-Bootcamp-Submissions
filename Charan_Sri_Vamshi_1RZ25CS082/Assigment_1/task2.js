525216a5c6828079447f2071d8d20c52c2457bf7
charan <vamshikommisetty@gmail.com>
2026-03-27 21:55:23 +0530

    charankommisetty six assignments

---
+++ Charan_Sri_Vamshi_1RZ25CS082/Assigment_1/task2.js
@@ -0,0 +1,13 @@
+// Arrow function for greet
+const greet = (name) => {
+    return "Hello, " + name + "!";
+};
+
+// Arrow function for addition
+const add = (a, b) => {
+    return a + b;
+};
+
+// Calling functions
+console.log(greet("harshit"));
+console.log("Sum:", add(10, 5));
---
+++ Charan_Sri_Vamshi_1RZ25CS082/Assigment_1/task3.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5];
+
+// Using arrow function inside map
+const doubled = numbers.map(num => num * 2);
+
+console.log(doubled);
